c2148306250997cd6762f0655a5ef7c29fc6bbd7
fenixzava <fenixzava@gmail.com>
2022-10-21 00:56:10 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] WriteArray()
+ {
+ int [] array = new int [4];
+ 
+ for(int i = 0; i < array.Length; i++)
+ {
+    array[i] = new Random().Next(0,10);
+ }
+ return array;
+ }
+
+void PrintArray( int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    System.Console.Write($" {array [i]}");
+    }
+}
+
+int SumOddIndexNumbers(int [] array)
+{
+int sum = 0;
+ for(int i = 0; i < array.Length; i++)
+ {
+    if(i%2 !=0)
+    {
+      sum= sum + array[i];  
+    }
+ }  
+ return sum; 
+}
+
+int [] answer = WriteArray();
+PrintArray(answer);
+int S = SumOddIndexNumbers(answer);
+System.Console.WriteLine($" ->{S}");
